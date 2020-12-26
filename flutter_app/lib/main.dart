8a3fe2fee89c02e3ee56694df071d183c957d51c
Shahfiq Shah <48479663+Gr3gg0r@users.noreply.github.com>
2020-12-26 16:29:28 +0800

    Third Commit

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,117 +1,205 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(KrenovatorApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class KrenovatorApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+          primaryColor: Colors.green, accentColor: Colors.lightGreenAccent),
+      title: "Krenovator Simple TodoApp",
+      home: CloudInit(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class CloudInit extends StatelessWidget {
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  Widget loadingScreen(){
+    return  Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  Future<void> initializeDefault() async {
+    FirebaseApp app = await Firebase.initializeApp();
+    assert(app != null);
+    print('Initialized default app $app');
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    return FutureBuilder(
+      future: initializeDefault(),
+      builder: (BuildContext context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting)
+          return loadingScreen();
+        else if(snapshot.connectionState == ConnectionState.done)
+          return HomePage();
+        else
+          return loadingScreen();
+      },
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("To Do App"),
+        centerTitle: true,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body: StreamBuilder(
+        stream: _firestore.collection("Categories").snapshots(),
+        builder: (BuildContext context , AsyncSnapshot<QuerySnapshot> snapshot){
+          if(snapshot.connectionState == ConnectionState.waiting)
+            return Center(child: CircularProgressIndicator(),);
+          return ListView(
+            children: snapshot.data.docs.map((DocumentSnapshot snapshot){
+              return Card(
+                child: ListTile(
+                  title: Text("${snapshot.data()['Categories']}"),
+                  trailing: IconButton(
+                    icon: Icon(Icons.delete,color: Colors.red,),
+                    onPressed:() => _firestore.collection("Categories").doc("${snapshot.id}").delete(),
+                  ),
+                  onTap: () => Navigator.push(context, MaterialPageRoute(
+                      builder: (BuildContext context) => CategoryHome(title: snapshot.data()['Categories'],)
+                  )),
+                ),
+              );
+            }).toList(),
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        onPressed: () => Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (BuildContext context) => AddCategory())),
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
+  }
+}
+
+class AddCategory extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomPadding: false,
+      appBar: AppBar(
+        title: Text("Add Category"),
+        centerTitle: true,
+      ),
+      body: AddCategoryForm(),
+    );
+  }
+}
+
+class AddCategoryForm extends StatefulWidget {
+
+  @override
+  _AddCategoryFormState createState() => _AddCategoryFormState();
+}
+
+class _AddCategoryFormState extends State<AddCategoryForm> {
+  final _fromkey = GlobalKey<FormState>();
+
+  String category;
+
+  bool loading=false;
+
+  FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  void _addData(context){
+    if(_fromkey.currentState.validate()){
+      setState(() {
+        loading = true;
+      });
+      _firestore.collection("Categories").add({
+        "Categories" : category
+      }).then((value) => Navigator.pop(context));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final double high = MediaQuery.of(context).size.height;
+    final double width = MediaQuery.of(context).size.width;
+    return Form(
+      key: _fromkey,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: "e.g Groceries",
+                    labelText: "Category",
+                    prefixIcon: Icon(Icons.list)),
+                validator: (val) {
+                  return val.isEmpty ? "Please enter valid text" : null;
+                },
+                onChanged: (val) => category = val,
+              ),
+              SizedBox(
+                height: high * 0.05,
+              ),
+              Container(
+                width: width * 0.9,
+                height: high * 0.07,
+                child: !loading?RaisedButton(
+                  color: Colors.greenAccent,
+                  child: Text(
+                    "Confirm",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: () => _addData(context),
+                ):Center(child: CircularProgressIndicator(),),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CategoryHome extends StatelessWidget {
+  final String title;
+  CategoryHome({this.title});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+        centerTitle: true,
+      ),
+      floatingActionButton:FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => Navigator.push(context, MaterialPageRoute(
+          builder: (BuildContext context) =>
+        )),
+      ),
+    );
+  }
+}
+
+class AddNotes extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
